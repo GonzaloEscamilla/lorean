833dba0e38a2ab3c29fc408590062564471f649d
GonzaloEscamilla <escamillagonzalo@gmail.com>
2023-12-19 18:39:56 +0000

    Deploying to gh-pages from @ GonzaloEscamilla/lorean@f98ec59ea942ec15e8ecccd1fbdc292d08a3a778 🚀

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-nuevo-destino-0.0.42";
+const cacheName = "DefaultCompany-nuevo-destino-0.0.43";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js.unityweb",
